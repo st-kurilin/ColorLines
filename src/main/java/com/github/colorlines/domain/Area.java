eb0581dcb665054e8ff27469d0031870a54fc7fe
Stanislav Kurilin <st.kurilin@gmail.com>
2011-11-27 13:10:34 +0200

    Move area size to position range.

--- src/main/java/com/github/colorlines/domain/Area.java
+++ src/main/java/com/github/colorlines/domain/Area.java
@@ -12,8 +12,4 @@ public interface Area {
      * @throws IllegalStateException if it doesn't contain any item at specified location
      */
     Ball take(Position location);
-
-    int getWidth();
-
-    int getHeight();
 }
