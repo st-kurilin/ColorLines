9a35125a939da32b29959a04ece4cb83ffe018c3
Stanislav Kurilin <stanislav.kurilin@teamdev.com>
2011-11-27 19:11:05 +0200

    Update module.

--- src/main/java/com/github/colorlines/domainimpl/DomainImplModule.java
+++ src/main/java/com/github/colorlines/domainimpl/DomainImplModule.java
@@ -1,14 +1,11 @@
 package com.github.colorlines.domainimpl;
 
-import com.github.colorlines.Game;
 import com.github.colorlines.consoleplayer.PlayerImpl;
 import com.github.colorlines.domain.*;
+import com.google.common.base.Predicate;
 import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Provides;
+import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
-
-import javax.inject.Singleton;
 
 /**
  * @author Stanislav Kurilin
@@ -18,25 +15,17 @@ public class DomainImplModule extends AbstractModule {
     protected void configure() {
         bindScope(GameScoped.class, new GameScope());
         bind(AreaCleaner.class).to(CleanHistorer.class);
+        bind(AreaCleaner.class).annotatedWith(Names.named("areaCleanerTarget")).to(PatternBasedCleaner.class);
+
         bind(BallGenerator.class).to(RandomBallGenerator.class);
         bindConstant().annotatedWith(Names.named("ballsGenerateByOneTime")).to(3);
-        bind(AreaCleaner.class).annotatedWith(Names.named("areaCleanerTarget")).to(PatternBasedCleaner.class);
-        bind(TurnValidator.class).toInstance(new TurnValidator() {
-            @Override
-            public boolean isValid(Area area, Turn turn) {
-                return true;
-            }
-        });
+
+        bind(TurnValidator.class).to(TurnValidatorImpl.class);
+        bind(new TypeLiteral<Predicate<Area>>() {
+        }).annotatedWith(Names.named("gameCanBeContinuePredicate")).to(GameCanBeContinuePredicate.class);
+
         bind(Player.class).to(PlayerImpl.class);
-    }
+        bind(Area.class).to(MutableArea.class);
 
-    @Provides
-    @Singleton
-    Area area(MutableArea area) {
-        return area;
-    }
-
-    public static void main(String[] args) {
-        Guice.createInjector(new DomainImplModule()).getInstance(Game.class).play();
     }
 }
