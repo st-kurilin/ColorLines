18c9d55ca5e1bc99df003dc9abb324b1562a0c16
Stanislav Kurilin <stanislav.kurilin@teamdev.com>
2011-11-27 19:09:20 +0200

    Add mutators

--- src/main/java/com/github/colorlines/domainimpl/MutableArea.java
+++ src/main/java/com/github/colorlines/domainimpl/MutableArea.java
@@ -6,6 +6,7 @@ import com.github.colorlines.domain.Position;
 import com.google.common.base.Function;
 import com.google.common.collect.Maps;
 
+import javax.inject.Singleton;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,26 +17,38 @@ import static java.lang.String.format;
 /**
  * @author Stanislav Kurilin
  */
-@GameScoped
-class MutableArea implements Area {
+@Singleton
+public class MutableArea implements Area {
     private final Set<Ball> balls = newHashSet();
-    private final Map<Position, Ball> ballsByPosition = Maps.uniqueIndex(balls, new Function<Ball, Position>() {
-        @Override
-        public Position apply(Ball input) {
-            return input.position();
-        }
-    });
+
+    private Map<Position, Ball> ballsByPosition() {
+        return Maps.uniqueIndex(balls, new Function<Ball, Position>() {
+            @Override
+            public Position apply(Ball input) {
+                return input.position();
+            }
+        });
+    }
+
     private final String TAKEN_WITH_WRONG_POSITION =
             "Can't deliver ball from specified location: %s Please make sure that ball exist using contains method";
 
     @Override
     public boolean contains(Position location) {
-        return ballsByPosition.containsKey(location);
+        return ballsByPosition().containsKey(location);
     }
 
     @Override
     public Ball take(Position location) {
-        checkState(ballsByPosition.containsKey(location), format(TAKEN_WITH_WRONG_POSITION, location));
-        return ballsByPosition.get(location);
+        checkState(ballsByPosition().containsKey(location), format(TAKEN_WITH_WRONG_POSITION, location));
+        return ballsByPosition().get(location);
+    }
+
+    public void addBalls(Set<Ball> added) {
+        balls.addAll(added);
+    }
+
+    public void removeBalls(Set<Ball> toRemove) {
+        balls.removeAll(toRemove);
     }
 }
