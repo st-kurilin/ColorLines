e5900136fa6e6f196d9721df739dbb02f9430443
Stanislav Kurilin <st.kurilin@gmail.com>
2011-12-01 23:33:30 +0200

    Workable ambiguous impl.

--- server-side/src/main/java/com/github/colorlines/domainimpl/PatternBasedCleaner.java
+++ server-side/src/main/java/com/github/colorlines/domainimpl/PatternBasedCleaner.java
@@ -1,28 +1,106 @@
 package com.github.colorlines.domainimpl;
 
-import com.github.colorlines.domain.Area;
-import com.github.colorlines.domain.AreaCleaner;
-import com.github.colorlines.domain.Ball;
-import com.github.colorlines.domain.Position;
+import com.github.colorlines.domain.*;
 import com.google.common.base.Function;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import com.google.common.collect.UnmodifiableIterator;
 
-import javax.annotation.Nullable;
 import javax.inject.Named;
 import java.util.Collection;
-import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
-import static com.google.common.collect.Lists.newLinkedList;
+import static com.github.colorlines.domain.Position.*;
+import static com.google.common.collect.Collections2.transform;
+import static com.google.common.collect.ImmutableList.of;
 
 /**
  * @author Stanislav Kurilin, Alex Lenkevich
  */
 @Named("areaCleanerTarget")
 public class PatternBasedCleaner implements AreaCleaner {
-    private final Collection<Cleaner> cleaners =
-            ImmutableList.of(HORIZONTAL, VERTICAL, DIAGONAL, DIAGONAL_BACK);
+    private final Collection<Cleaner> cleaners = transform(of(new Function<Position, UnmodifiableIterator<Position>>() {
+                @Override
+                public UnmodifiableIterator<Position> apply(final Position position) {
+                    return new UnmodifiableIterator<Position>() {
+                        Position curent = position;
+
+                        @Override
+                        public boolean hasNext() {
+                            return X_VALUES.contains(curent.getX() + 1);
+                        }
+
+                        @Override
+                        public Position next() {
+                            return curent = create(curent.getX() + 1, curent.getY());
+                        }
+                    };
+                }
+            }, new Function<Position, UnmodifiableIterator<Position>>() {
+                @Override
+                public UnmodifiableIterator<Position> apply(final Position position) {
+                    return new UnmodifiableIterator<Position>() {
+                        Position curent = position;
+
+                        @Override
+                        public boolean hasNext() {
+                            return Y_VALUES.contains(curent.getY() + 1);
+                        }
+
+                        @Override
+                        public Position next() {
+                            return curent = create(curent.getX(), curent.getY() + 1);
+                        }
+                    };
+                }
+            }, new Function<Position, UnmodifiableIterator<Position>>() {
+                @Override
+                public UnmodifiableIterator<Position> apply(final Position position) {
+                    return new UnmodifiableIterator<Position>() {
+                        Position curent = position;
+
+                        @Override
+                        public boolean hasNext() {
+                            return X_VALUES.contains(curent.getX() + 1) && Y_VALUES.contains(curent.getY() + 1);
+                        }
+
+                        @Override
+                        public Position next() {
+                            return curent = create(curent.getX() + 1, curent.getY() + 1);
+                        }
+                    };
+                }
+            }, new Function<Position, UnmodifiableIterator<Position>>() {
+        @Override
+        public UnmodifiableIterator<Position> apply(final Position position) {
+            return new UnmodifiableIterator<Position>() {
+                Position curent = position;
+
+                @Override
+                public boolean hasNext() {
+                    return X_VALUES.contains(curent.getX() - 1) && Y_VALUES.contains(curent.getY() + 1);
+                }
+
+                @Override
+                public Position next() {
+                    return curent = create(curent.getX() - 1, curent.getY() + 1);
+                }
+            };
+        }
+    }
+    ), new Function<Function<Position, UnmodifiableIterator<Position>>, Cleaner>() {
+        @Override
+        public Cleaner apply(final Function<Position, UnmodifiableIterator<Position>> input) {
+            return new Cleaner(new IteratorFactory() {
+                @Override
+                public UnmodifiableIterator<Position> iterator(Position init) {
+                    return input.apply(init);
+                }
+            });
+        }
+    });
 
     @Override
     public Set<Ball> clean(Area area) {
@@ -33,145 +111,45 @@ public class PatternBasedCleaner implements AreaCleaner {
         return builder.build();
     }
 
-    interface Cleaner extends Function<Area, Set<Ball>> {
+
+    interface IteratorFactory {
+        UnmodifiableIterator<Position> iterator(Position init);
     }
 
-    private static final int minBallsToDestroy = 5;
+    private static class Cleaner implements Function<Area, Set<Ball>> {
+        final IteratorFactory iteratorFactory;
 
-    private final static Cleaner HORIZONTAL = new Cleaner() {
+        private Cleaner(IteratorFactory iteratorFactory) {
+            this.iteratorFactory = iteratorFactory;
+        }
+
+        final Iterable<Position> positions = transform(Sets.cartesianProduct(X_VALUES, Y_VALUES), new Function<List<Integer>, Position>() {
+            @Override
+            public Position apply(List<Integer> integers) {
+                return create(integers.get(0), integers.get(1));
+            }
+        });
+
         @Override
         public Set<Ball> apply(Area area) {
             final ImmutableSet.Builder<Ball> result = ImmutableSet.builder();
-            for (int y = Position.HEIGHT_RANGE.lowerEndpoint(); Position.HEIGHT_RANGE.contains(y); y++) {
-                final LinkedList<Ball> candidate = newLinkedList();
-                for (int x = Position.WIDTH_RANGE.lowerEndpoint(); Position.WIDTH_RANGE.contains(x); x++) {
-                    Position curPosition = Position.create(x, y);
-                    if (!area.contains(curPosition)) {
-                        if (candidate.size() >= minBallsToDestroy) {
-                            result.addAll(candidate);
-                        }
-                        candidate.clear();
-                        continue;
-                    }
-                    final Ball ball = area.take(curPosition);
-                    if (candidate.isEmpty() || candidate.getFirst().color().equals(ball.color())) {
-                        candidate.add(ball);
-                    } else {
-                        if (candidate.size() >= minBallsToDestroy) {
-                            result.addAll(candidate);
-                        }
-                        candidate.clear();
-                    }
+            for (Position init : positions) {
+                if (!area.contains(init)) continue;
+                final Iterator<Position> it = iteratorFactory.iterator(init);
+                final ImmutableSet.Builder<Ball> builder = ImmutableSet.builder();
+                builder.add(area.take(init));
+                final Color color = area.take(init).color();
+                while (it.hasNext()) {
+                    final Position next = it.next();
+                    if (!area.contains(next) || area.take(next).color() != color) break;
+                    builder.add(area.take(next));
                 }
-                if (candidate.size() >= minBallsToDestroy) {
-                    result.addAll(candidate);
-                }
-                candidate.clear();
+                final ImmutableSet<Ball> series = builder.build();
+                if (series.size() >= 5) result.addAll(series);
             }
             return result.build();
         }
-    };
-    private final static Cleaner VERTICAL = new Cleaner() {
-        @Override
-        public Set<Ball> apply(@Nullable Area area) {
-            final ImmutableSet.Builder<Ball> result = ImmutableSet.builder();
-            for (int x = Position.WIDTH_RANGE.lowerEndpoint(); Position.WIDTH_RANGE.contains(x); x++) {
-                final LinkedList<Ball> candidate = newLinkedList();
-                for (int y = Position.HEIGHT_RANGE.lowerEndpoint(); Position.HEIGHT_RANGE.contains(y); y++) {
-                    Position curPosition = Position.create(x, y);
-                    if (!area.contains(curPosition)) {
-                        if (candidate.size() >= minBallsToDestroy) {
-                            result.addAll(candidate);
-                        }
-                        candidate.clear();
-                        continue;
-                    }
-                    final Ball ball = area.take(curPosition);
-                    if (candidate.isEmpty() || candidate.getFirst().color().equals(ball.color())) {
-                        candidate.add(ball);
-                    } else {
-                        if (candidate.size() >= minBallsToDestroy) {
-                            result.addAll(candidate);
-                        }
-                        candidate.clear();
-                    }
-                }
-                if (candidate.size() >= minBallsToDestroy) {
-                    result.addAll(candidate);
-                }
-                candidate.clear();
-            }
-            return result.build();
-        }
-    };
-    private final static Cleaner DIAGONAL = new Cleaner() {
-        @Override
-        public Set<Ball> apply(@Nullable Area area) {
-            final ImmutableSet.Builder<Ball> result = ImmutableSet.builder();
-            for (int x = Position.WIDTH_RANGE.lowerEndpoint(); Position.WIDTH_RANGE.contains(x); x++) {
-                final LinkedList<Ball> candidate = newLinkedList();
-                for (int y = Position.HEIGHT_RANGE.lowerEndpoint(); Position.HEIGHT_RANGE.contains(y); y++) {
-                    for (int i = 0; i < Math.max(Position.HEIGHT_RANGE.upperEndpoint(), Position.HEIGHT_RANGE.upperEndpoint()); i++) {
-                        if (!Position.WIDTH_RANGE.contains(x + i) || !Position.HEIGHT_RANGE.contains(y + i) || !area.contains(Position.create(x + i, y + i))) {
-                            if (candidate.size() >= minBallsToDestroy) {
-                                result.addAll(candidate);
-                            }
-                            candidate.clear();
-                            continue;
-                        }
-                        Position curPosition = Position.create(x + i, y + i);
-                        final Ball ball = area.take(curPosition);
-                        if (candidate.isEmpty() || candidate.getFirst().color().equals(ball.color())) {
-                            candidate.add(ball);
-                        } else {
-                            if (candidate.size() >= minBallsToDestroy) {
-                                result.addAll(candidate);
-                            }
-                            candidate.clear();
-                        }
-                    }
-                }
-                if (candidate.size() >= minBallsToDestroy) {
-                    result.addAll(candidate);
-                }
-                candidate.clear();
-            }
-            return result.build();
-        }
-    };
-    private final static Cleaner DIAGONAL_BACK = new Cleaner() {
-        @Override
-        public Set<Ball> apply(@Nullable Area area) {
-            final ImmutableSet.Builder<Ball> result = ImmutableSet.builder();
-            for (int x = Position.WIDTH_RANGE.lowerEndpoint(); Position.WIDTH_RANGE.contains(x); x++) {
-                final LinkedList<Ball> candidate = newLinkedList();
-                for (int y = Position.HEIGHT_RANGE.lowerEndpoint(); Position.HEIGHT_RANGE.contains(y); y++) {
-                    for (int i = 0; i < Math.max(Position.HEIGHT_RANGE.upperEndpoint(), Position.HEIGHT_RANGE.upperEndpoint()); i++) {
-                        if (!Position.WIDTH_RANGE.contains(x - i) || !Position.HEIGHT_RANGE.contains(y + i) || !area.contains(Position.create(x - i, y + i))) {
-                            if (candidate.size() >= minBallsToDestroy) {
-                                result.addAll(candidate);
-                            }
-                            candidate.clear();
-                            continue;
-                        }
-                        Position curPosition = Position.create(x - i, y + i);
-                        final Ball ball = area.take(curPosition);
-                        if (candidate.isEmpty() || candidate.getFirst().color().equals(ball.color())) {
-                            candidate.add(ball);
-                        } else {
-                            if (candidate.size() >= minBallsToDestroy) {
-                                result.addAll(candidate);
-                            }
-                            candidate.clear();
-                        }
-                    }
-                }
-                if (candidate.size() >= minBallsToDestroy) {
-                    result.addAll(candidate);
-                }
-                candidate.clear();
-            }
-            return result.build();
-        }
-    };
+    }
+
+    ;
 }
