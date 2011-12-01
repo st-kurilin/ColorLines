2415ddaed695f5732d805aba8b4244605fb2e244
Stanislav Kurilin <st.kurilin@gmail.com>
2011-12-01 23:34:10 +0200

    Iterable to set.

--- domain/src/main/java/com/github/colorlines/domain/Position.java
+++ domain/src/main/java/com/github/colorlines/domain/Position.java
@@ -3,6 +3,8 @@ package com.github.colorlines.domain;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Range;
 
+import java.util.Set;
+
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.collect.DiscreteDomains.integers;
 import static com.google.common.collect.Ranges.closed;
@@ -16,8 +18,8 @@ public final class Position {
     @Deprecated //use iterator
     public static Range<Integer> HEIGHT_RANGE = closed(0, 8);
 
-    public static Iterable<Integer> X_VALUES = closed(0, 8).asSet(integers());
-    public static Iterable<Integer> Y_VALUES = closed(0, 8).asSet(integers());
+    public static Set<Integer> X_VALUES = closed(0, 8).asSet(integers());
+    public static Set<Integer> Y_VALUES = closed(0, 8).asSet(integers());
 
     private final int x, y;
 
